873fe6ded1685a900078300ff18e53de06a01795
mrampiah <mrampiah@Ebenezers-MacBook-Pro.local>
2016-04-05 17:01:07 -0400

    added main app class

---
+++ DHMC-HealthApp/src/dhmc/MainApp.java
@@ -0,0 +1,73 @@
+package dhmc;
+
+import java.io.IOException;
+
+import dhmc.controller.DatabaseHandler;
+import dhmc.view.LoginController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+	private DatabaseHandler dbHandler;
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("DHMC - Health App 3.0");
+
+		initRootLayout();
+
+		showLogin();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public DatabaseHandler getDatabseHandler(){
+		return dbHandler;
+	}
+	
+
+	public void initRootLayout() {
+		try {
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void showLogin() {
+		try {
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/Login.fxml"));
+			AnchorPane login = (AnchorPane) loader.load();
+			
+			primaryStage.setScene(new Scene(login));
+
+			LoginController controller = loader.getController();
+			controller.setMainApp(this);
+
+//			primaryStage.setWidth(400);
+//			primaryStage.setHeight(300);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
