9fba8f932650733386353854c0922cb4f6fe21a1
seanrgbriggs <seanrgbriggs@gmail.com>
2016-04-07 12:30:47 -0400

    The Controller (almost done)
    All the logic, none of the implementation, to be fixed post haste

---
+++ DHMC-HealthApp/src/view/ImportExportController.java
@@ -0,0 +1,55 @@
+package application;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+//import model.mainApp;
+
+public class ImportExportController {
+	// private MainApp mainApp;
+
+	@FXML
+	private Button fileButton;
+	@FXML
+	private Button importButton;
+	@FXML
+	private Button exportButton;
+
+	private File curFile;
+	//	private LinkedList<Patient> pts;
+	
+	
+	/*
+	 * public void setMainApp(MainApp mainApp) { this.mainApp = mainApp; }
+	 */
+
+	@FXML
+	private void initialize() {
+
+	}
+
+	public void chooseFile() {
+		FileChooser fc = new FileChooser();
+		fc.setTitle("Select CSV to import");
+		curFile = fc.showOpenDialog(null);
+		if (curFile.exists())
+			importButton.setText("Import " + curFile.getName());
+	}
+/*
+	public void importCSV() {
+		if (curFile != null && curFile.exists())
+			pts = model.CSVParsingUtils.CSVImport(curFile);
+	}
+
+	public void exportCSV(){
+		int curFileInt = 1;
+		while (	Files.exists(Paths.get("exported"+1+".csv")))
+			curFileInt++;
+		model.CSVParsingUtils.CSVExport("exported"+1+".csv", pts);
+	}
+	*/
+}
