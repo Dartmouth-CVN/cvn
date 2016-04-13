66a81a5387ae17c28ca8330ff6d274bdce018f61
Luke Buquicchio <ljbuquicchio@wpi.edu>
2016-04-13 19:07:19 -0400

    V2.0

--- DHMC-HealthApp/Test/controler/FitBitTest.java
+++ DHMC-HealthApp/Test/controler/FitBitTest.java
@@ -13,8 +13,8 @@ public class FitBitTest {
 	@Test
 	public void test() {
 		File fitbitData = new File("/Users/Luke/Desktop/fitbitExport.csv");
-		LinkedList<HealthInfo> hi = new LinkedList<HealthInfo>();
-		hi = FitBitParsingUtils.fitBitImport(fitbitData);
+		//LinkedList<HealthInfo> hi = new LinkedList<HealthInfo>();
+		//hi = FitBitParsingUtils.fitBitImport(fitbitData);
 		assertEquals(FitBitParsingUtils.fitBitImport(fitbitData).size(), 31);
 		//System.out.println(hi.get(0).getDate() + "\n" + hi.get(0).getWeight() + "\n" + hi.get(0).getCaloriesBurned() + "\n" + hi.get(0).getMinAsleep());
 	}
