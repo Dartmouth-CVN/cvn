5652312a351e4a207e548b11435430f9936fdb00
mrampiah <mrampiah@EKAMPIAH-02.WIFI.WPI.EDU>
2016-04-15 15:48:46 -0400

    testing folder

---
+++ DHMC-HealthApp/test/controller/DatabaseHandlerTest.java
@@ -0,0 +1,14 @@
+package controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DatabaseHandlerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
