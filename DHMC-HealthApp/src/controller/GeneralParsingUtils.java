85cc606341982ec16f9e18e25cca70b663edcaeb
Luke Buquicchio <ljbuquicchio@wpi.edu>
2016-04-18 13:09:22 -0400

    Generalized parsing utils with sean

--- DHMC-HealthApp/src/controller/GeneralParsingUtils.java
+++ DHMC-HealthApp/src/controller/GeneralParsingUtils.java
@@ -2,10 +2,13 @@ package controller;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Scanner;
 
 import model.MainApp;
+import model.MedicalStaff;
+import model.Medication;
 import model.Patient;
 
 @SuppressWarnings("unused")
@@ -107,6 +110,117 @@ public abstract class GeneralParsingUtils {
 
 	public static String[] splitSepValuesLineAndRemoveCommasFromVal(String s, String delimiter) {
 		return splitSepValuesLine(s, delimiter, true);
+	}
+
+	/**
+	 * This function will check to see if a given string contains any nonwhitespace or noncomma characters
+	 * 
+	 * 
+	 * @param s: the string to check
+	 * @return a boolean that will be true if any valid characters are detected
+	 */
+	static boolean hasData(String s) {
+		int counter = 0;
+		for(int i = 0; i < s.length(); i++) {
+			if(Character.isWhitespace(s.charAt(i)) || s.charAt(i) == ',') {
+				counter++;
+			}
+		}
+		return counter != s.length();
+	}
+
+	/**
+	 * Writes a String of XML in the format < tag >content< /tag >
+	 * 
+	 * @param tag
+	 *            the tag to envelop the content
+	 * @param content
+	 *            the content to be enveloped
+	 * @return the String in XML format
+	 */
+	protected static String XMLLine(String tag, String content) {
+		return XMLLine(tag, content, false);
+	}
+
+	/**
+	 * Writes a String of XML in the format < tag >content< /tag > with newlines
+	 * if major is true
+	 * 
+	 * @param tag
+	 *            the tag to envelop the content
+	 * @param content
+	 *            the content to be enveloped
+	 * @param major
+	 *            whether or not the XML should be formatted such that
+	 *            multi-line structures look nicer, by adding newlines
+	 * @return the XMLified String
+	 */
+	protected static String XMLLine(String tag, String content, boolean major) {
+		String output = (tag != null) ? "<" + tag + ">" : "";
+		if (major)
+			output += "\n\n";
+		output += content;
+		if (major)
+			output += "\n";
+		if (tag == null)
+			output += "\n";
+		else if (tag.split(" ").length > 1)
+			output += "</" + tag.split(" ")[0] + ">\n";
+		else
+			output += "</" + tag + ">\n";
+		return output;
+	}
+
+	/**
+	 * Given an array of Strings to each be parsed by one sub-tag, surround that
+	 * list with a main tag
+	 * 
+	 * @param tag
+	 *            the tag to surround the whole list
+	 * @param subtag
+	 *            the tag to surround each entry in the list
+	 * @param content
+	 *            the entries for the list
+	 * @return the String in XML format
+	 */
+	protected static String XMLList(String tag, String subtag, Iterable<String> content) {
+		String inner = "";
+		for (String c : content)
+			inner += XMLLine(subtag, c) + "<br>";
+		return XMLLine(tag, inner, true);
+	}
+
+	protected static String XMLList(String tag, String subtag, String[] content) {
+		return XMLList(tag, subtag, Arrays.asList(content));
+	}
+
+	/**
+	 * Given a LinkedList of MedicalStaff, produce the appropriate XML String
+	 * 
+	 * @param staffMembers
+	 *            the LinkedList to provide
+	 * @return the String in XML format
+	 */
+	protected static String[] XMLParseMedStaff(LinkedList<MedicalStaff> staffMembers) {
+		String[] output = new String[staffMembers.size()];
+		for (int i = 0; i < output.length; i++)
+			output[i] = staffMembers.get(i).getUserID();
+	
+		return output;
+	}
+
+	/**
+	 * Given a LinkedList of Medication, produce the appropriate XML String
+	 * 
+	 * @param meds
+	 *            the LinkedList to provide
+	 * @return the String in XML format
+	 */
+	private static String[] XMLParseMedication(LinkedList<Medication> meds) {
+		String[] output = new String[meds.size()];
+		for (int i = 0; i < output.length; i++)
+			output[i] = meds.get(i).getName();
+		return output;
 	}
 
 }
