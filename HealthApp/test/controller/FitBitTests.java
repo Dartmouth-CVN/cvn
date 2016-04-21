c3b87d1756a4582ebd1ba114d0321121954ad64e
mrampiah <mrampiah@Ebenezers-MacBook-Pro.local>
2016-04-21 10:59:24 -0400

    + .class

--- HealthApp/test/controller/FitBitTests.java
+++ HealthApp/test/controller/FitBitTests.java
@@ -3,7 +3,11 @@ package controller;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Set;
+
+import javax.swing.JFileChooser;
 
 import org.junit.Test;
 
@@ -15,11 +19,16 @@ public class FitBitTests {
 
 	@Test
 	public void test() {
-		File fitbitData = new File("/Users/Luke/Desktop/fitbitExport.csv");
-		LinkedList<HealthInfo> hi = new LinkedList<HealthInfo>();
-		hi = FitBitParsingUtils.fitBitImport(fitbitData);
-		assertEquals(FitBitParsingUtils.fitBitImport(fitbitData).size(), 31);
-		System.out.println(hi.get(0).getDate() + "\n" + hi.get(0).getWeight() + "\n" + hi.get(0).getCaloriesBurned() + "\n" + hi.get(0).getMinAsleep());
+		JFileChooser fc = new JFileChooser();
+		int returnVal = fc.showOpenDialog(null);
+
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+    		File fitbitData = fc.getSelectedFile();
+    		Set<HealthInfo> hi = new HashSet<HealthInfo>();
+    		hi = FitBitParsingUtils.fitBitImport(fitbitData);
+    		assertEquals(FitBitParsingUtils.fitBitImport(fitbitData).size(), 31);
+//    		System.out.println(hi.get(0).getDate() + "\n" + hi.get(0).getWeight() + "\n" + hi.get(0).getCaloriesBurned() + "\n" + hi.get(0).getMinAsleep());
+        } 
 	}
 
 }
