6e20a0f1d6b80ff81c5ac294e7e042204a19edef
mnnguyen16 <mnnguyen@wpi.edu>
2016-05-01 13:24:41 -0400

    added sorting methods for health attributes

--- HealthApp - Project/src/model/HealthProfile.java
+++ HealthApp - Project/src/model/HealthProfile.java
@@ -10,6 +10,24 @@ public class HealthProfile{
     List<String> dietaryRestrictions;
     String userId;
 
+    //sorted HealthAttributes
+    private List weightList = new LinkedList<>();
+    private List bmiList = new LinkedList<>();
+    private List fatList = new LinkedList<>();
+    private List caloriesBurnedList = new LinkedList<>();
+    private List stepsList = new LinkedList<>();
+    private List distanceList = new LinkedList<>();
+    private List floorsList = new LinkedList<>();
+    private List minSedentaryList = new LinkedList<>();
+    private List minLightlyActiveList = new LinkedList<>();
+    private List minFairlyActiveList = new LinkedList<>();
+    private List minVeryActiveList = new LinkedList<>();
+    private List activityCaloriesList = new LinkedList<>();
+    private List minAsleepList = new LinkedList<>();
+    private List minAwakeList = new LinkedList<>();
+    private List numbAwakeningsList = new LinkedList<>();
+    private List timeInBedList = new LinkedList<>();
+
     public HealthProfile() {
         this(0, "", new LinkedList<>(), new LinkedList<>(), new LinkedList<>());
     }
@@ -20,6 +38,7 @@ public class HealthProfile{
         this.healthInfo = healthInfo;
         this.allergies = allergies;
         this.dietaryRestrictions = dietaryRestrictions;
+
     }
 
     public void addNewHealthInfo(HealthAttribute<?> info) {
@@ -77,5 +96,34 @@ public class HealthProfile{
     public int hashCode() {
         return healthInfo.hashCode() * allergies.hashCode() * dietaryRestrictions.hashCode();
     }
+
+    public void separateCategories(List<HealthAttribute<?>> healthInfo){
+        for (HealthAttribute i: healthInfo){
+            addToCategory(i);
+        }
+        return;
+    }
+
+    public void addToCategory(HealthAttribute info){
+        switch(info.getName()){
+            case "Weight" : weightList.add(info); return;
+            case "BMI" : bmiList.add(info); return;
+            case "Fat" : fatList.add(info); return;
+            case "Calories Burned" : caloriesBurnedList.add(info); return;
+            case "Steps" : stepsList.add(info); return;
+            case "Distance" : distanceList.add(info); return;
+            case "Floors" : floorsList.add(info); return;
+            case "Minutes Sedentary" : minSedentaryList.add(info); return;
+            case "Minutes Lightly Active" : minLightlyActiveList.add(info); return;
+            case "Minutes Fairly Active" : minFairlyActiveList.add(info); return;
+            case "Minutes Very Active" : minVeryActiveList.add(info); return;
+            case "Activity Calories" : activityCaloriesList.add(info); return;
+            case "Minutes Asleep" : minAsleepList.add(info); return;
+            case "Minutes Awake" : minAwakeList.add(info); return;
+            case "Number of Awakenings" : numbAwakeningsList.add(info); return;
+            case "Time in Bed" : timeInBedList.add(info); return;
+        }
+    }
+
 
 }
